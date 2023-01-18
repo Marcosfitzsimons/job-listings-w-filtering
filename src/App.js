d22578921791e1b239818653489fd13bda071b31
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-01-18 18:31:32 -0300

    Fix job list filter problem when deleting a filter tag

--- src/App.js
+++ src/App.js
@@ -7,39 +7,38 @@ function App() {
   const [jobList, setJobList] = useState(data);
   const [filteredTags, setFilteredTags] = useState([]);
 
-  // Filter job listings
 
   const filterJobs = filteredTags => {
     if (filteredTags.length > 0) {
-      const filteredJobs = jobList.filter(job => {
+      const filteredJobs = data.filter(job => {
         const tags = [...job.languages, ...job.tools, job.role, job.level];
         return filteredTags.every(filteredTag => tags.includes(filteredTag.text));
       });
-      setJobList(filteredJobs);
+      return filteredJobs;
     } else {
-      return;
+      return data;
     }
   };
-
-  // Clear filtered tags
-
-  const clearFilteredTags = () => {
-    setFilteredTags([]);
-    setJobList(data);
-  };
-
-  // Delete filter
 
   const handleDeleteFilter = (tag) => {
     const newFilteredTags = filteredTags.filter((filter) => {
       return filter.text !== tag.target.textContent;
     });
     setFilteredTags(newFilteredTags);
-  }
+    setJobList(filterJobs(newFilteredTags))
+  };
+
+
+  const clearFilteredTags = () => {
+    setFilteredTags([]);
+    setJobList(data);
+  };
+
 
   useEffect(() => {
-    filterJobs(filteredTags);
+    setJobList(filterJobs(filteredTags));
   }, [filteredTags]);
+
 
   return (
     <div className="App h-full">
